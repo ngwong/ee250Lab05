03867747286fb74993856c1f938a69bd5c0b8fee
Nilay Pachuari <pachauri@usc.edu>
2018-02-20 17:10:42 -0800

    Added blink function

---
+++ test.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+led = 11
+
+def blink():
+	GPIO.setup(led, GPIO.OUT)
+	for i in range (0, 5):
+		GPIO.output(led, GPIO.HIGH)
+		time.sleep(0.1)
+		GPIO.output(led, GPIO.LOW)
+		time.sleep(0.1)	
+
+def Main():
+	GPIO.setmode(GPIO.BOARD)
+
+if __name__ = '__main__':
+	Main()
