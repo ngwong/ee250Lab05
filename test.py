e28ece17b258d60f4d9b10df9b934694dedc1a22
Nilay Pachuari <pachauri@usc.edu>
2018-02-20 18:22:24 -0800

    Comment Code added

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ MOSI = 10
 CS   = 8
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
-#
+# MCP3008 Channels
 light = 0
 sound = 1
 
@@ -26,7 +26,8 @@ def init():
 	GPIO.setwarnings(False)
 	GPIO.setup(led, GPIO.OUT)
 
-#delay in seconds and amount of times to blink
+# delay in seconds and amount of times to blink
+# takes 2*delay to run
 def blink(delay, amount):
 	for i in range (amount):
 		GPIO.output(led, GPIO.HIGH)
@@ -34,11 +35,13 @@ def blink(delay, amount):
 		GPIO.output(led, GPIO.LOW)
 		time.sleep(delay)	
 
+# takes "delay" to run
 def blink_once(delay):
 	GPIO.output(led, GPIO.HIGH)
 	time.sleep(delay)
 	GPIO.output(led, GPIO.LOW)
 
+# Prints "amount" number of values every "delay" seconds
 def read_light(delay, amount):
 	for i in range (amount):
 		light_val = mcp.read_adc(light)
@@ -48,6 +51,8 @@ def read_light(delay, amount):
 			print (str(light_val) + " Dark")
 		time.sleep(delay)
 
+# Prints "amount" number of values every "delay" seconds
+# Binks the LED once the length of "delay" if sensor is tapped
 def read_sound(delay, amount):
 	for i in range (amount):
 		sound_val = mcp.read_adc(sound)
@@ -58,6 +63,7 @@ def read_sound(delay, amount):
 		else:
 			time.sleep(delay)
 
+# TestSuite
 def Main():
 	init()
 	blink(.5, 5)
@@ -66,5 +72,6 @@ def Main():
 	read_sound(.1, 50)
 	blink(.2, 4)
 
+# Necessary for python3 terminal call
 if __name__ == '__main__':
 	Main()
